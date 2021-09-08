f6704982ad8a3eb5c023f64076dc4e0b4bd86487
SanskritiPandey <khushiksp17@gmail.com>
2021-09-08 15:17:04 -0700

    feat: updated the code

--- app.js
+++ app.js
@@ -2,11 +2,14 @@ const billAmount = document.querySelector("#bill-amount");
 const cashgGiven =  document.querySelector("#cash-amount");
 const checkbutton = document.querySelector("#check-btn");
 const msg = document.querySelector("#error-msg");
+const availableNotes =[2000,500,100,20,10,5,1];
+const notes = document.querySelectorAll(".notes");
 
 checkbutton.addEventListener("click", function checkBillandCashAmount(){
     if(billAmount.value>0){
-        if(billAmount.value<= cashgGiven.value){
+        if(cashgGiven.value <= billAmount.value){
             const amountToReturned = cashgGiven.value - billAmount.value;
+            // console.log(amountToReturned);
             calculateChange(amountToReturned);
 
         }
@@ -18,9 +21,13 @@ checkbutton.addEventListener("click", function checkBillandCashAmount(){
         msg.innerHTML= "The bill amount must be greator than zero.";
     }
 });
-function calculateChange()
+function calculateChange(amountToReturned)
 {
-
+for(let i =0;i<availableNotes.length; i++){
+    const noOFNotes = Math.trunc(amountToReturned/availableNotes[i]);
+    amountToReturned = amountToReturned% availableNotes[i];
+    noOFNotes[i].innerText = notes;
+}
 }
 
 
