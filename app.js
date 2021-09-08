d8bef2c6abd53dfe5a06090b753685954a6774e0
SanskritiPandey <khushiksp17@gmail.com>
2021-09-08 14:13:27 -0700

    feat: functions updated

--- app.js
+++ app.js
@@ -1,3 +1,26 @@
 const billAmount = document.querySelector("#bill-amount");
+const cashgGiven =  document.querySelector("#cash-amount");
+const checkbutton = document.querySelector("#check-btn");
+const msg = document.querySelector("#error-msg");
 
-const checkButton = console.log(billAmount.value);
+checkbutton.addEventListener("click", function checkBillandCashAmount(){
+    if(billAmount.value>0){
+        if(billAmount.value<= cashgGiven.value){
+            const amountToReturned = cashgGiven.value - billAmount.value;
+            calculateChange(amountToReturned);
+
+        }
+        else{
+            msg.innerHTML  = "cash given should be at least equal to the bill amount";
+        }
+}
+    else{
+        msg.innerHTML= "The bill amount must be greator than zero.";
+    }
+});
+function calculateChange()
+{
+
+}
+
+
