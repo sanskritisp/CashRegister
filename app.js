a05e81c19d27fbb3d907198aec40ad0fb7c4c5e6
SanskritiPandey <khushiksp17@gmail.com>
2021-09-08 17:49:38 -0700

    feat: code completed

--- app.js
+++ app.js
@@ -1,22 +1,24 @@
 const billAmount = document.querySelector("#bill-amount");
-const cashgGiven =  document.querySelector("#cash-amount");
+const cashGiven =  document.querySelector("#cash-amount");
 const checkbutton = document.querySelector("#check-btn");
 const msg = document.querySelector("#error-msg");
 const availableNotes =[2000,500,100,20,10,5,1];
 const notes = document.querySelectorAll(".notes");
 
 checkbutton.addEventListener("click", function checkBillandCashAmount(){
-    if(billAmount.value>0){
-        if(cashgGiven.value <= billAmount.value){
-            const amountToReturned = cashgGiven.value - billAmount.value;
-            // console.log(amountToReturned);
+    let billAmountVal =parseFloat(billAmount.value)
+    let cashGivenVal =parseFloat(cashGiven.value)
+    if(billAmountVal>0){
+        if(cashGivenVal >= billAmountVal){
+            const amountToReturned = cashGivenVal - billAmountVal;
+            console.log(amountToReturned);
             calculateChange(amountToReturned);
 
         }
         else{
             msg.innerHTML  = "cash given should be at least equal to the bill amount";
         }
-}
+    }
     else{
         msg.innerHTML= "The bill amount must be greator than zero.";
     }
@@ -25,8 +27,8 @@ function calculateChange(amountToReturned)
 {
 for(let i =0;i<availableNotes.length; i++){
     const noOFNotes = Math.trunc(amountToReturned/availableNotes[i]);
-    amountToReturned = amountToReturned% availableNotes[i];
-    noOFNotes[i].innerText = notes;
+    amountToReturned = amountToReturned % availableNotes[i];
+    notes[i].innerText =  noOFNotes;
 }
 }
 
